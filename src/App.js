460dbaebc2e14eaf5b3f1f07c93e6ea0b80aeb96
Junfeng ou <junfengou@gmail.com>
2020-08-09 10:52:57 -0500

    Adding the project and login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+
+// A wrapper that handles all the communication to spotify and react
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null); 
+
+  // Run code based on a given condition
+  // Come back here and do more research 
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = ""; //hide the access token for security purposes
+
+    const _token = hash.access_token
+
+    if(_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      //getMe method gets the end user info and you could display it in console
+      spotify.getMe().then(user => {
+        console.log("person: " , user)
+      }); // get the user account
+    }
+
+    console.log("I have a token ", token);
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*Spotify logo */}
+      {/* Login with spotify button */}
+
+      {
+        token ? (
+          <Player />
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Login.css';
+import { loginUrl } from './spotify';
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="login">
+                {/* Spotify Logo*/}
+                <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+                {/*Login with spotify button */}
+                <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
+            </div>
+        );
+    }
+}
+
+export default Login;
