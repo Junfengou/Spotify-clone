cb387ec11ac9b07c10396852dbd0a82c8fbbc8bf
Junfeng ou <junfengou@gmail.com>
2020-08-09 11:39:37 -0500

    Adding React Context API functionality

---
+++ src/reducer.js
@@ -0,0 +1,30 @@
+//Creating the inital data layer 
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null
+};
+
+// state is how it currently look which means it could look like initialState or something else
+// action is what modify the situation
+const reducer = (state, action) => {
+    console.log(action); //debug 
+    
+    // action -> type, [payload]
+
+    switch(action.type) {
+        case 'SET_USER':
+            return {
+                //keep whatever is in current state, update the user slice with whatever the user.action was
+                ...state,
+                user: action.user
+            }
+            //if nothing changes, return the state
+        default: 
+            return state;
+    }
+};
+
+
+export default reducer;
